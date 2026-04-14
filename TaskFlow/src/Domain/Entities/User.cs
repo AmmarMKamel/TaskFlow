3f3b3569d9e955d272e0e27ad24ab7093735cef9
AmmarMKamel <ammarkamel9899@gmail.com>
2026-04-14 23:51:00 +0200

    feat: Add User and RefreshToken entities

---
+++ TaskFlow/src/Domain/Entities/User.cs
@@ -0,0 +1,22 @@
+﻿namespace TaskFlow.src.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public string PasswordHash { get; set; } = null!;
+        public string PasswordSalt { get; set; } = null!;
+
+        public bool IsActive { get; set; } = true;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+
+        public List<RefreshToken> RefreshTokens { get; set; } = new();
+    }
+}
