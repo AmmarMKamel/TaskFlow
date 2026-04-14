699b595697ece143ed681b1220897ee6b203f45d
AmmarMKamel <ammarkamel9899@gmail.com>
2026-04-15 01:06:17 +0200

    feat: add PasswordHasher service for hashing passwords with salt

---
+++ TaskFlow/src/Infrastructure/Services/PasswordHasher.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace TaskFlow.src.Infrastructure.Services
+{
+    public class PasswordHasher
+    {
+        public (string hash, string salt) HashPassword(string password)
+        {
+            using var hmac = new HMACSHA512();
+
+            var salt = Convert.ToBase64String(hmac.Key);
+            var hash = Convert.ToBase64String(hmac.ComputeHash(Encoding.UTF8.GetBytes(password)));
+
+            return (hash, salt);
+        }
+    }
+}
