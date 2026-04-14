cb6f50cac8bd5f94cad8d4b51bb948d96c9997fe
AmmarMKamel <ammarkamel9899@gmail.com>
2026-04-15 01:10:45 +0200

    feat: add AuthController with user registration endpoint

---
+++ TaskFlow/src/API/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskFlow.src.Application.DTOs.Auth;
+using TaskFlow.src.Application.Interfaces;
+
+namespace TaskFlow.src.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService ?? throw new ArgumentNullException(nameof(authService));
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterRequestDto request)
+        {
+            var result = await _authService.RegisterAsync(request);
+            return Ok(result);
+        }
+    }
+}
