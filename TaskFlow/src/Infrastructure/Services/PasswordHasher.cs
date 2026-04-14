d55731c854121a93defd2ab807fa90819748eaf4
AmmarMKamel <ammarkamel9899@gmail.com>
2026-04-15 01:43:34 +0200

    feat: add VerifyPassword method to PasswordHasher service

--- TaskFlow/src/Infrastructure/Services/PasswordHasher.cs
+++ TaskFlow/src/Infrastructure/Services/PasswordHasher.cs
@@ -14,5 +14,16 @@ namespace TaskFlow.src.Infrastructure.Services
 
             return (hash, salt);
         }
+
+        public bool VerifyPassword(string password, string storedHash, string storedSalt)
+        {
+            var key = Convert.FromBase64String(storedSalt);
+
+            using var hmac = new HMACSHA512(key);
+
+            var computedHash = Convert.ToBase64String(hmac.ComputeHash(Encoding.UTF8.GetBytes(password)));
+
+            return computedHash == storedHash;
+        }
     }
 }
