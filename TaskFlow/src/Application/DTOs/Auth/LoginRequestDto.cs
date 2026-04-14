cf67855d869c8ed642d9d8146b08a3f4b3b92d4e
AmmarMKamel <ammarkamel9899@gmail.com>
2026-04-15 01:45:26 +0200

    feat: add login request and response DTOs

---
+++ TaskFlow/src/Application/DTOs/Auth/LoginRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskFlow.src.Application.DTOs.Auth
+{
+    public class LoginRequestDto
+    {
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
---
+++ TaskFlow/src/Application/DTOs/Auth/LoginResponseDto.cs
@@ -0,0 +1,7 @@
+﻿namespace TaskFlow.src.Application.DTOs.Auth
+{
+    public class LoginResponseDto
+    {
+        public string AccessToken { get; set; } = null!;
+    }
+}
